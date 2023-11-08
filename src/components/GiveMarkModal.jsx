475dc6c1a735c255eb6354ea990f68d8e085b1a4
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-11-08 23:03:53 +0600

    pdf preview added in give marks modal with react-pdf

--- src/components/GiveMarkModal.jsx
+++ src/components/GiveMarkModal.jsx
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types';
 import { axiosInstance } from '../hooks/useAxios';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { GlobalContext } from '../context/ContextProvider';
 import toast from 'react-hot-toast';
 import { useMutation } from '@tanstack/react-query';
+import {Document, Page, pdfjs} from 'react-pdf';
+import { FaCircleXmark } from 'react-icons/fa6';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
 
 const GiveMarkModal = ({showModal, setShowModal, assignment, refetch}) => {
   const {user} = useContext(GlobalContext);
+  const [numPages, setNumPages] = useState();
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
   const updateFetcher = async(updatedAssignment) => {
     const res = await axiosInstance.put(`/submitted-assignments?id=${assignment?._id}`, updatedAssignment, {headers: {Authorization: user?.email}});
@@ -37,7 +49,8 @@ const GiveMarkModal = ({showModal, setShowModal, assignment, refetch}) => {
 
   return (
     <section className="fixed w-screen h-screen left-0 right-0 bg-black bg-opacity-40 flex justify-center items-center transition-[top] duration-300 z-40" style={showModal ? {top: "0px"} : {top: "-120%"}}>
-      <form className="bg-white w-full max-w-[600px] m-6 p-6 rounded-lg" onSubmit={handleSubmit}>
+      <form className="formTag bg-white w-full max-w-[700px] max-h-[calc(100vh_-_2rem)] m-6 p-6 rounded-lg overflow-auto relative" onSubmit={handleSubmit}>
+      <FaCircleXmark className="text-3xl text-primary absolute right-6 top-6 cursor-pointer" onClick={() => setShowModal(false)} />
         <h2 className="text-3xl font-medium mb-6 text-center">Give Mark</h2>
         <p className='text-left mb-2'><span className="font-bold">PDF Link:</span> <a className='text-blue-700 underline' href={assignment?.pdfLink} target="_blank" rel="noopener noreferrer">{assignment?.pdfLink}</a></p>
         {
@@ -52,6 +65,19 @@ const GiveMarkModal = ({showModal, setShowModal, assignment, refetch}) => {
         <div className="flex justify-center items-center gap-2">
           <button className="btn btn-primary" type="submit">Submit</button>
           <button className="btn btn-primary btn-outline" onClick={() => setShowModal(false)} type='button'>Close</button>
+        </div>
+
+        <div className='overflow-x-auto'>
+          <div className='mt-8 flex justify-center w-[635px]'>
+            <Document file={assignment?.pdfLink} onLoadSuccess={onDocumentLoadSuccess}>
+              {Array.apply(null, Array(numPages)).map((x, i) => i+1).map(page => {
+                return (
+                  <Page key={page} pageNumber={page} renderTextLayer={false} renderAnnotationLayer={false} />
+                );
+              })}
+              
+            </Document>
+          </div>
         </div>
       </form>
     </section>
--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001",
-  // baseURL: "https://study-hub-server-1.vercel.app",
+  baseURL: "https://study-hub-server-1.vercel.app",
   withCredentials: true
 })
